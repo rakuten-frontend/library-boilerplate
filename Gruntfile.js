1a1c4e553d790634514d6c091217787ecf8212fd
Tanjo, Hiroyuki <expheno@gmail.com>
2015-02-03 15:43:12 +0900

    Revert "Switch release task from grunt-bump to grunt-release"
    This reverts commit 21aa1aeda5c7df92b1b5f1b212d9fe42e55a4e8c.

--- Gruntfile.js
+++ Gruntfile.js
@@ -148,42 +148,20 @@ module.exports = function (grunt) {
       }
     },
 
-    release: {
+    bump: {
       options: {
-        changelogText: '### <%= version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n',
-        file: 'package.json',
-        additionalFiles: ['bower.json'],
-        indentation: '  ',
-        folder: '.',
-        tagName: 'v<%= version %>',
-        commitMessage: 'Release v<%= version %>',
-        tagMessage: 'Release v<%= version %>'
-      },
-      bump: {
-        options: {
-          bump: true,
-          changelog: true,
-          add: false,
-          commit: false,
-          tag: false,
-          push: false,
-          pushTags: false,
-          npm: false,
-          npmtag: false
-        }
-      },
-      commit: {
-        options: {
-          bump: false,
-          changelog: false,
-          add: true,
-          commit: true,
-          tag: true,
-          push: true,
-          pushTags: true,
-          npm: true,
-          npmtag: false
-        }
+        files: ['package.json', 'bower.json'],
+        updateConfigs: ['pkg'],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['-a'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Release v%VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false
       }
     }
 
@@ -193,13 +171,12 @@ module.exports = function (grunt) {
   grunt.registerTask('build', ['clean', 'sass', 'autoprefixer', 'concat', 'uglify', 'cssmin']);
   grunt.registerTask('default', ['test', 'autoprefixer', 'concat', 'uglify', 'cssmin']);
 
-  grunt.registerTask('publish', 'Release package after test, build and bump.', function (type) {
-    type = type || 'patch';
+  grunt.registerTask('release', 'Test, build and bump package.', function (type) {
     grunt.task.run([
       'test',
-      'release:bump:' + type,
+      'bump-only:' + (type || 'patch'),
       'build',
-      'release:commit:' + type
+      'bump-commit'
     ]);
   });
 
